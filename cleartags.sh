1881fb93135ac40aa0b4eeb4209ae0fbf0233244
Jon Pentland <jon.pentland@pretagov.co.uk>
2023-09-12 17:13:00 +0100

    require github token

--- cleartags.sh
+++ cleartags.sh
@@ -10,20 +10,29 @@ PR=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
 echo "repo=${REPO}/pulls/${PR}" >> $GITHUB_OUTPUT
 
 # Get commits on this PR
-shas=$(curl -L \
+shas_raw=$(curl -L \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer ${GITHUB_TOKEN}" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/${REPO}/pulls/${PR}/commits | jq '.[].sha'| tr -d '"')
+  https://api.github.com/repos/${REPO}/pulls/${PR}/commits)
+
+echo "RAW Shas: $shas_raw"
+
+echo "sha=${shas_raw}" >> $GITHUB_OUTPUT
+
+shas=`echo ${shas_raw} | jq '.[].sha'| tr -d '"'`
 
 # Get tags
-tags=$(curl -L \
+tags_raw=$(curl -L \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer ${GITHUB_TOKEN}" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/${REPO}/tags | jq '.[] | "\(.commit.sha) \(.name)"'|tr -d '"')
+  https://api.github.com/repos/${REPO}/tags)
+  
 
-echo "tags=$tags" >> $GITHUB_OUTPUT
+echo "tags=${tags_raw}" >> $GITHUB_OUTPUT
+tags=`echo ${tags_raw} | jq '.[] | "\(.commit.sha) \(.name)"'|tr -d '"'
+
 
 # Work out which tags to delete
 tagsToDelete=()
